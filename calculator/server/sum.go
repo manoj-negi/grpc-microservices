1187c2c0f2414d0b2c306771b67c59cd40ee22f9
Manoj Singh Negi <www.msnegi@gmail.com>
2023-03-31 11:47:35 +0530

    fix

--- calculator/server/sum.go
+++ calculator/server/sum.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 
-	pb "github.com/Clement-Jean/grpc-go-course/calculator/proto"
+	pb "github.com/manoj-negi/grpc-microservices/calculator/proto"
 )
 
 func (s *Server) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error) {
